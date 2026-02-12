3dc142e13809b59412c99a3eb9efedc51f13258c
lucasdelacale <corp.lucasmartins@gmail.com>
2026-02-12 18:08:57 -0300

    data types

--- pyclass/1th-data-types.py
+++ pyclass/1th-data-types.py
@@ -1,11 +1,15 @@
 #Dynamic typing of Python
 
-#String - Simple text
+#String - simple text
 print("Hello world!")
+print(  type("Hello world!")    )
+
 
 #Int - whole numbers
 print(1312)
+print(  type(1312)  )
 
 #Float - floating-point numbers
 print(1.3, 1.2)
+print(  type(1.3),   type(1.2) )
 
