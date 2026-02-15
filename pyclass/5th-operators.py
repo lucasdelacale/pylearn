a9a77f315b749d03552903607b4cc59e679adb1b
lucasdelacale <corp.lucasmartins@gmail.com>
2026-02-15 12:53:01 -0300

    5th operators

---
+++ pyclass/5th-operators.py
@@ -0,0 +1,29 @@
+# Arithmetic operators
+
+# Addition
+addition = 10 + 10
+print('Addition:', addition)
+
+# Subtraction
+subtraction = 10 - 5
+print('Subtraction:', subtraction)
+
+# Multiplication
+multiplication = 10 * 10
+print('Multiplication:', multiplication)
+
+# Division
+division = 10 / 5
+print('Division:', division)
+
+# Division of integers
+integers_division = 10 // 2.5
+print('Division of integers:', integers_division)
+
+# Exponentiation
+exponentiation = 10 ** 2
+print('Exponentiation:', exponentiation)
+
+# Absolute value
+absolute = 15 % 2 #Rest of division
+print('Absolute value:', absolute)
