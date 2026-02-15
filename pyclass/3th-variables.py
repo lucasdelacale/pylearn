8361bb26307352ac1f0ee9a1b6a26fdf3f4e1591
lucasdelacale <corp.lucasmartins@gmail.com>
2026-02-15 12:07:02 -0300

    usage of variables and examples

---
+++ pyclass/3th-variables.py
@@ -0,0 +1,19 @@
+#Variables are used to store something in the program's memory
+#PEP8: Start with lowercase letters, and you can use numbers and underlines
+#The equals sign (=) is the assignment operator
+#Usage: variable_name = expression
+
+#Example
+full_name = 'Lucas Martins'
+print(full_name)
+print()
+
+#Exercise
+first_name = 'Lucas'
+age = 27
+of_legal_age = age >= 18
+
+#Usage
+print(first_name, 'é maior de idade?', of_legal_age)
+
+
