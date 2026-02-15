69681abed5e84be47a8c1756321482cf2bc7dc58
lucasdelacale <corp.lucasmartins@gmail.com>
2026-02-15 12:23:34 -0300

    Posting of the exercise on variables and its solution

---
+++ pyclass/4th-exc-variables.py
@@ -0,0 +1,17 @@
+# Resolution
+name = 'Lucas'
+last_name = 'Martins'
+age = 27
+year_of_birth = 1998
+legal_of_age = age >= 18
+height = 1.78
+
+#Exercise
+print('Name:', name)
+print('Last name:', last_name)
+print('Age:', age)
+print('Year of birth:', year_of_birth)
+print('Is legal of age?', legal_of_age)
+print('Height in meters:', height)
+
+
