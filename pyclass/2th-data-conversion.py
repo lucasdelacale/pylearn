f6a1bb6cf3f4b01752b8c7dfe3f9d06da59b5309
lucasdelacale <corp.lucasmartins@gmail.com>
2026-02-15 11:44:20 -0300

    coercion exercise

---
+++ pyclass/2th-data-conversion.py
@@ -0,0 +1,17 @@
+#Typeconvertion, typecasting and coercion
+#Convert one type to another
+#Immutable and primitive types
+#Str, int, float, bool
+
+#Example
+print(1+2)
+print('a'+'b')
+print()
+
+#Example with coercion
+print(1, type(1))
+print('1', type('1'))
+
+#Coercion
+print(int('1'), type(int('1')))
+print(1 + int('1'))
